8e40b970d8bde3157102f794626df625ae73efc4
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-06-29 22:18:54 +0200

    [add] kilometre feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,29 @@
 class HomeController < ApplicationController
   def index
-
   end
   
   def distance
-    required = [:type, :distance]
-    form_complete = true
-    required.each do |f|
-      if params.has_key? f and not params[f].blank?
-        # that's good news. do nothing
-      else
-        form_complete = false
-      end
-      if form_complete
-        form_status_msg = 'Thank you for your feedback!'
-      else
-        form_status_msg = 'Please fill in all the remaining form fields and resubmit.'
-      end
-      format.html { render :home, locals: { status_msg: form_status_msg } }
+    @distance = params[:distance].to_i
+    @type = params[:type]
+    @car = 1.93
+    @train = 0.5
+    @bus = 1.03
+    # if type = car , then @co2 = @distance * 0.62
+    # if type = train , then @co2 = @distance * 0.42
+    # if type = bus , then @co2 = @distance * 0.32
+    if @type == 'car'
+      @co2 = (@distance * @car)
+    elsif @type == 'train'
+      @co2 = (@distance * @train)
+    elsif @type == 'bus'
+      @co2 = (@distance * @bus)
     end
+    puts  @co2
+    puts @status_msg
+    render 'index'
   end
+
+  
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/home', to: "home#index"
-  post '/home/distance', to: "home#distance", as: 'distance'
-
+  # post '/home/distance', to: "home#distance", as: 'distance'
+  post '/home', to: "home#distance", as: 'distance'
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
